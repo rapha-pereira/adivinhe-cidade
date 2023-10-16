a155b2c6015273177276721617254b4c78de7142
rapha-pereira <raphaelpgomes1@gmail.com>
2023-10-15 23:26:02 -0300

    refactored version

--- backend/handler.js
+++ backend/handler.js
@@ -1,23 +1,5 @@
 // Constants
 const IBGE_SPREADSHEET_ID = "1dcri2Nj4FIdmL-bX94eNYygMdzHuriLRpDfL4wcRDHU";
-const IBGE_API_BASE_URL = "https://servicodados.ibge.gov.br/api/v3/malhas/municipios/";
-
-// MIsc.
-function getScriptURL() {
-  return ScriptApp.getService().getUrl();
-}
-
-// Normalize accents
-function removeAccents(str) {
-  const accentMap = {
-    'á': 'a', 'â': 'a', 'ã': 'a', 'à': 'a',
-    'é': 'e', 'ê': 'e', 'í': 'i', 'ó': 'o',
-    'ô': 'o', 'õ': 'o', 'ú': 'u', 'ü': 'u',
-    'ç': 'c'
-  };
-
-  return str.replace(/[áâãàéêíóôõúüç]/g, matched => accentMap[matched] || matched);
-}
 
 // Retrieve IBGE data
 let ibgeData;
@@ -40,33 +22,9 @@ function getIBGERandomCityObject() {
   return filteredData[randomIndex];
 }
 
-// Build & Get functions
-function buildCityURL(data) {
-  return `${IBGE_API_BASE_URL}${data['municipio-id']}`;
-}
-
-function getCityName(data) {
-  const cityName = data['municipio-nome'];
-  console.log(cityName)
-  return removeAccents(cityName).toLowerCase();
-}
-
-function getCityStateName(data) {
-  return data['UF-nome'];
-}
-
-function getCityRegionName(data) {
-  return data['mesorregiao-nome'];
-}
-
+// Main
 function doGet() {
-  const ibgeCityObj = getIBGERandomCityObject();
-
-  const template = HtmlService.createTemplateFromFile('index');
-  template.imageUrl = buildCityURL(ibgeCityObj);
-  template.cityName = getCityName(ibgeCityObj);
-  template.cityStateName = getCityStateName(ibgeCityObj);
-  template.cityRegionName = getCityRegionName(ibgeCityObj);
+  const template = HtmlService.createTemplateFromFile('app/mainpage');
 
   return template.evaluate()
     .setTitle('AdivinheCidade')
